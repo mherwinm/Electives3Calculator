f025870f2644bcebf14adecf49cd6fbb6742cdb1
mherwinm <mherwinm@gmail.com>
2020-01-11 12:28:55 +0800

    Functionality 1-9

--- Form1.vb
+++ Form1.vb
@@ -14,4 +14,44 @@
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
 
     End Sub
+
+    Private Sub Button29_Click(sender As Object, e As EventArgs) Handles Button29.Click
+        Label1.Text = Label1.Text & "0"
+    End Sub
+
+    Private Sub Button23_Click(sender As Object, e As EventArgs) Handles Button23.Click
+        Label1.Text = Label1.Text & "1"
+    End Sub
+
+    Private Sub Button22_Click(sender As Object, e As EventArgs) Handles Button22.Click
+        Label1.Text = Label1.Text & "2"
+    End Sub
+
+    Private Sub Button21_Click(sender As Object, e As EventArgs) Handles Button21.Click
+        Label1.Text = Label1.Text & "3"
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        Label1.Text = Label1.Text & "4"
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        Label1.Text = Label1.Text & "5"
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        Label1.Text = Label1.Text & "6"
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Label1.Text = Label1.Text & "7"
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Label1.Text = Label1.Text & "8"
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Label1.Text = Label1.Text & "9"
+    End Sub
 End Class
